80cf964d8593b69ad04c770f611d551f91ce79e0
serdarsen <serdarsendev@gmail.com>
2025-06-14 15:07:59 -0400

    feat: add solution for "remove duplicates from sorted array" problem.

---
+++ 0026.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        l = 1;
+        for r in range(1, len(nums)):
+            if nums[r] != nums[r - 1]:
+                nums[l] = nums[r];
+                l += 1;
+        return l;
+
+# case 1
+nums = [1,1,2];
+
+solution = Solution();
+
+length = solution.removeDuplicates(nums);
+
+print(f"{length}, nums: {nums[:length]}")
+
+# case 2
+nums = [0,0,1,1,1,2,2,3,3,4];
+
+solution = Solution();
+
+length = solution.removeDuplicates(nums);
+
+print(f"{length}, nums: {nums[:length]}")
