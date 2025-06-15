d5ed62b123f7c801826e797c18448bd0356b6e9f
serdarsen <serdarsendev@gmail.com>
2025-06-15 17:02:56 -0400

    feat: add solution for "Best Time to Buy and Sell Stock II" problem

--- 0026.py
+++ 0026.py
@@ -1,26 +1,20 @@
 class Solution(object):
     def removeDuplicates(self, nums):
-        l = 1;
+        l = 1
         for r in range(1, len(nums)):
             if nums[r] != nums[r - 1]:
-                nums[l] = nums[r];
-                l += 1;
-        return l;
+                nums[l] = nums[r]
+                l += 1
+        return l
+
+solution = Solution()
 
 # case 1
-nums = [1,1,2];
-
-solution = Solution();
-
-length = solution.removeDuplicates(nums);
-
+nums = [1,1,2]
+length = solution.removeDuplicates(nums)
 print(f"{length}, nums: {nums[:length]}")
 
 # case 2
-nums = [0,0,1,1,1,2,2,3,3,4];
-
-solution = Solution();
-
-length = solution.removeDuplicates(nums);
-
+nums = [0,0,1,1,1,2,2,3,3,4]
+length = solution.removeDuplicates(nums)
 print(f"{length}, nums: {nums[:length]}")
---
+++ 0122.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+      profit = 0
+
+      for i in range(1, len(prices)):
+        if prices[i] > prices[i - 1]:
+          profit += (prices[i] - prices[i - 1])
+    
+      return profit
+
+
+solution = Solution()
+
+# case 1
+prices = [7,1,5,3,6,4]
+maxProfit = solution.maxProfit(prices)
+print(maxProfit)
+
+# case 2
+prices = [1,2,3,4,5]
+maxProfit = solution.maxProfit(prices)
+print(maxProfit)
+
+# case 3
+prices = [7,6,4,3,1]
+maxProfit = solution.maxProfit(prices)
+print(maxProfit)
