4fa8f0f932a552e50325a866bf862369c0a81e3d
serdarsen <serdarsendev@gmail.com>
2025-06-23 03:33:11 -0400

    feat: add TypeScript implementation of rotate function for in-place array rotation

--- 0189.js
+++ 0189.js
@@ -30,6 +30,10 @@ var rotate = function(nums, k) {
   }
 };
 
+export { 
+  rotate 
+};
+
 // case 1
 let nums = [1,2,3,4,5,6,7];
 rotate(nums, 3);
---
+++ 0189.ts
@@ -0,0 +1,43 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function rotate(nums: number[], k: number): void {
+    k = k % nums.length;
+    let l = 0; 
+    let r = nums.length - 1;
+    while (l < r) {
+        [nums[l], nums[r]] = [nums[r], nums[l]];
+        l++;
+        r--;
+    }
+
+    l = 0;
+    r = k - 1;
+    while (l < r) {
+        [nums[l], nums[r]] = [nums[r], nums[l]];
+        l++;
+        r--;
+    }
+
+    l = k;
+    r = nums.length - 1;
+    while (l < r) {
+        [nums[l], nums[r]] = [nums[r], nums[l]];
+        l++;
+        r--;
+    }
+}
+
+export { 
+  rotate 
+};
+
+// case 1
+let nums: number[] = [1,2,3,4,5,6,7];
+rotate(nums, 3);
+console.log(nums);
+
+// case 2
+nums = [-1,-100,3,99];
+rotate(nums, 2);
+console.log(nums);
