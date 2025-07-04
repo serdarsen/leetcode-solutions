1a1c5203f507cb79d1364a498d4a05f8eb0afced
serdarsen <serdarsendev@gmail.com>
2025-07-04 19:56:58 -0400

    fix: refactor 66 py renaming carry

--- 0066.py
+++ 0066.py
@@ -3,18 +3,19 @@ from typing import List
 class Solution:
   def plusOne(self, digits: List[int]) -> List[int]:
       digits = digits[::-1]
-      one, i = 1, 0
+      carry = 1
+      i = 0
 
-      while one:
+      while carry:
         if i < len(digits):
           if digits[i] == 9:
             digits[i] = 0
           else:
             digits[i] += 1
-            one = 0
+            carry = 0
         else:
           digits.append(1)
-          one = 0
+          carry = 0
         i += 1
       return digits[::-1]
 
