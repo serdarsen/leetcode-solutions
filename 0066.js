a24d2aa4833fdfa51ce619580aa3563717c3d9e4
serdarsen <serdarsendev@gmail.com>
2025-07-05 16:36:40 -0400

    refactor: eliminate reverse by iterating from end and using unshift to handle carry overflow

--- 0066.js
+++ 0066.js
@@ -3,39 +3,44 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-  digits.reverse();
   let carry = 1;
-  let i = 0;
 
-  while (carry) {
-    if (i < digits.length) {
-      if (digits[i] === 9) {
-        digits[i] = 0;
-      } else {
-        digits[i] += 1;
-        carry = 0;
-      }
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (carry === 0) {
+      break;
+    }
+
+    if (digits[i] === 9) {
+      digits[i] = 0;
     } else {
-      digits.push(1);
+      digits[i] += 1;
       carry = 0;
     }
-    i++;
   }
 
-  return digits.reverse();    
+  if (carry === 1) {
+    digits.unshift(1);
+  }
+
+  return digits;
 };
 
 // case 1
 let digits = [1, 2, 3];
 let result = plusOne(digits);
-console.log(result);
+console.log(result); // [1, 2, 4]
 
 // case 2
 digits = [4, 3, 2, 1];
-result = plusOne(digits)
-console.log(result);
+result = plusOne(digits);
+console.log(result); // [4, 3, 2, 2]
 
 // case 3
 digits = [9];
 result = plusOne(digits);
-console.log(result);
+console.log(result); // [1, 0]
+
+// case 4
+digits = [1, 9, 4];
+result = plusOne(digits);
+console.log(result); // [1, 9, 5]
