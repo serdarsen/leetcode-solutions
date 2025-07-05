bf8002d89b6a3d3ab8684b28e58bfc6ad4c0ffc7
serdarsen <serdarsendev@gmail.com>
2025-07-05 16:45:30 -0400

    feat: add plus one typescript

---
+++ 0066.ts
@@ -0,0 +1,40 @@
+function plusOne(digits: number[]): number[] {
+  let carry = 1;
+
+  for (let i = digits.length - 1; i >= 0 && carry; i--) {
+    if (digits[i] === 9) {
+      digits[i] = 0;
+    } else {
+      digits[i] += 1;
+      carry = 0;
+    }
+  }
+
+  if (carry) digits.unshift(1);
+
+  return digits;
+}
+
+export {
+  plusOne
+}
+
+// case 1
+let digits = [1, 2, 3];
+let result = plusOne(digits);
+console.log(result); // [1, 2, 4]
+
+// case 2
+digits = [4, 3, 2, 1];
+result = plusOne(digits);
+console.log(result); // [4, 3, 2, 2]
+
+// case 3
+digits = [9];
+result = plusOne(digits);
+console.log(result); // [1, 0]
+
+// case 4
+digits = [1, 9, 4];
+result = plusOne(digits);
+console.log(result); // [1, 9, 5]
