ecfd33d882496eff979976c8e482101097b95a5c
serdarsen <serdarsendev@gmail.com>
2025-07-12 19:42:35 -0400

    feat: implement move zeroes algorithm

---
+++ 0283.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l = 0
+        for r in range(len(nums)):
+          if nums[r]:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+        print(nums)
+
+solution = Solution()
+
+# case 1
+digits = [0, 1, 0, 3, 12]
+solution.moveZeroes(digits) # [1, 3, 12, 0, 0]
+
+# case 2
+digits = [0]
+solution.moveZeroes(digits) # [0]
